2733a9e3c435c3670597c61a3fca07baee3d7556
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-19 09:53:10 +0500

    validatio and error handling

--- src/components/svelte/hello-world.svelte
+++ src/components/svelte/hello-world.svelte
@@ -2,18 +2,27 @@
 import { default as events } from "../../config/events";
 import { useRef, useStore, useEffect } from "../../hooks/core.svelte";
 import { socketStore } from "../../stores/socket";
-import type { ChatRoomJoinResponse } from "../../lib/types";
+import {
+	ChatRoomJoinResponseSchema,
+	type ChatRoomJoinResponse,
+} from "../../validators/chat.validators";
 
-const chatRoom = useRef<ChatRoomJoinResponse | null>(null);
+const chatRoom = useRef<ChatRoomJoinResponse["room"]>(null);
 
 useStore(socketStore, async (socket) => {
 	try {
 		if (socket) {
-			const response = await socket.emitWithAck(events.chat.room.join, {
+			const { error, data } = await socket.emitWithAck(events.chat.room.join, {
 				otherUserId: "aa624179-5634-431b-96ea-5bd0f083bf69",
 			});
 
-			chatRoom.value = response.data.room;
+			if (error) {
+				throw new Error(error);
+			}
+
+			const parsedResponse = ChatRoomJoinResponseSchema.parse(data);
+
+			chatRoom.value = parsedResponse.room;
 		}
 	} catch (error) {
 		console.error(error);
